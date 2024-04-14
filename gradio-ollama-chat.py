3d62c533d5486f6f2cd37643cb61dba1012455df
ramwar <ramon.wartala@sinnerschrader.com>
2024-04-14 18:18:10 +0200

    add gradio ollama app

--- gradio-ollama-chat.py
+++ gradio-ollama-chat.py
@@ -12,38 +12,6 @@ def generate(prompt, context, top_k, top_p, temp):
     response = llm.invoke(prompt)
     return response.content, context
 
-# def generate(prompt, context, top_k, top_p, temp):
-#     r = requests.post('http://localhost:11434/api/generate',
-#                      json={
-#                          'model': model,
-#                          'prompt': prompt,
-#                          'context': context,
-#                          'options':{
-#                              'top_k': top_k,
-#                              'temperature':top_p,
-#                              'top_p': temp
-#                          }
-#                      },
-#                      stream=False)
-#     r.raise_for_status()
-
- 
-#     response = ""  
-
-#     for line in r.iter_lines():
-#         body = json.loads(line)
-#         response_part = body.get('response', '')
-#         print(response_part)
-#         if 'error' in body:
-#             raise Exception(body['error'])
-
-#         response += response_part
-
-#         if body.get('done', False):
-#             context = body.get('context', [])
-#             return response, context
-
-
 def chat(input, chat_history, top_k, top_p, temp):
 
     chat_history = chat_history or []
